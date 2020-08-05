e2a480255fed1048728deabdde62a86f9526fbcc
tuc01108 <tuc01108@temple.edu>
2020-08-04 19:40:15 -0500

    speller work

--- lectures/c/int/strBinInt.c
+++ lectures/c/int/strBinInt.c
@@ -19,24 +19,27 @@ int main(void)
     int array[n], i;
     int m = (int)(*(word));
     int *p1 = array;
-    
-    for(i = 0; m > 0; i++)    
-    {    
-        array[i] = m % 2;    
-        m = m / 2;    
+
+    for(i = 0; m > 0; i++)
+    {
+        array[i] = m % 2;
+        m = m / 2;
     }
     // We optionally print the binary
     printBinary(p1, i);
-    
+
+    int j = (int)(*(word)) % 97;
+    printf("%d\n", j);
+
     free(word);
     return 0;
 }
 
 int printBinary(int *p1, int i)
 {
-    for(i = i - 1; i >= 0; i--)    
-    {    
-        printf("%d", *(p1 + i));    
+    for(i = i - 1; i >= 0; i--)
+    {
+        printf("%d", *(p1 + i));
     }
     printf("\n");
     return 0;
